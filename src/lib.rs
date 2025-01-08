64eaf22e16820627954c9f70be3a30884b4fa09f
Jing Peng <pj.hades@gmail.com>
2025-01-07 21:09:25 -0500

    Kind of works

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Error};
-use git2::{BranchType, ErrorCode, Remote, Repository};
+use git2::{BranchType, ErrorCode, Reference, Remote, Repository};
+use git_url_parse::GitUrl;
 use nvim_oxi::api::opts::CreateCommandOpts;
 use nvim_oxi::api::types::{CommandArgs, CommandNArgs, CommandRange};
 use nvim_oxi::{api, print};
@@ -8,7 +9,7 @@ use nvim_oxi::{api, print};
 fn nvim_repolink() -> Result<(), Error> {
     let opts = CreateCommandOpts::builder()
         .bang(true)
-        .nargs(CommandNArgs::Zero)
+        .nargs(CommandNArgs::ZeroOrOne)
         .range(CommandRange::Count(0))
         .build();
 
@@ -25,85 +26,159 @@ fn nvim_repolink() -> Result<(), Error> {
     Ok(())
 }
 
-fn split_shorthand(shorthand: &str) -> (&str, &str) {
-    let parts = shorthand.split('/').collect::<Vec<&str>>();
-    (parts[0], parts[1])
+enum GitObject {
+    Branch(String),
+    Tag(String),
+    Commit(String),
 }
 
-// https://github.com/pjhades/jujube/blob/master/kernel/src/memory/heap.rs#L5
-//         ---------- remote url
-//                    -------------- remote url
-//                                        ------ remote branch
-//                                               ------------------------- path, relative to repo root
-//                                                                         -- nvim
 fn generate_repolink(args: CommandArgs) -> Result<(), Error> {
-    let path = api::get_current_buf()
-        .get_name()?
+    let repo = Repository::discover(std::env::current_dir()?)?;
+    let remote_name = args.args.unwrap_or("origin".to_string());
+    let remote = repo.find_remote(&remote_name)?;
+    let head = repo.head()?;
+
+    if head.is_note() || head.is_tag() || head.is_remote() {
+        return Err(anyhow!("HEAD points directly to a note, tag or remote"));
+    }
+
+    let gitobj = if repo.head_detached()? {
+        search_references(&repo, |r| {
+            if !r.is_tag() {
+                return None;
+            }
+            std::str::from_utf8(r.shorthand_bytes())
+                .ok()
+                .map(|s| GitObject::Tag(s.to_string()))
+        })?
+        .or_else(|| {
+            head.peel_to_commit()
+                .ok()
+                .map(|commit| GitObject::Commit(commit.id().to_string()))
+        })
+    } else if head.is_branch() {
+        get_remote_branch(&repo, &remote_name)?
+    } else {
+        return Err(anyhow!("HEAD is neither a branch nor detached"));
+    }
+    .ok_or(anyhow!("Cannot figure out branch, tag or commit"))?;
+
+    let repo_path = repo.workdir().ok_or(anyhow!("Repository is bare"))?;
+    let file_path = api::get_current_buf().get_name()?;
+    let rel_path = file_path
+        .strip_prefix(repo_path)
+        .map_err(|e| anyhow!("Failed to figure out relative path of current buffer: {e}"))?
+        .to_path_buf()
         .into_os_string()
         .into_string()
         .unwrap_or_else(|s| s.as_os_str().to_string_lossy().to_string());
-
     let range = if args.range == 0 {
         None
     } else {
         Some((args.line1, args.line2))
     };
 
-    let repo = Repository::discover(std::env::current_dir()?)?;
-    let head = repo.head()?;
+    let url = GitUrl::parse(std::str::from_utf8(remote.url_bytes())?)?;
 
-    let remote = if head.is_branch() {
-        let name = std::str::from_utf8(head.shorthand_bytes())?;
-        let branch = repo.find_branch(name, BranchType::Local)?;
-
-        match branch.upstream() {
-            Ok(upstream) => {
-                let shorthand = std::str::from_utf8(upstream.name_bytes()?)?;
-                let (remote, _) = split_shorthand(shorthand);
-                repo.find_remote(remote)?
-            }
-            Err(e) if e.code() == ErrorCode::NotFound => {
-                // Current branch doesn't track any remote ones. Try to search for
-                // its tip commit in all the remote references.
-                locate_commit(&repo)?.ok_or(anyhow!("not found"))?
-            }
-            Err(e) => {
-                return Err(Error::from(e));
-            }
-        }
-    } else if repo.head_detached()? || head.is_tag() {
-        locate_commit(&repo)?.ok_or(anyhow!("not found"))?
-    } else {
-        return Err(anyhow!("HEAD is neither a branch nor a tag"));
-    };
-
-    print!("{}", remote.url().unwrap());
+    let link = make_link(url, gitobj, rel_path, range)?;
+    print!("{link}");
 
     Ok(())
 }
 
-fn locate_commit(repo: &Repository) -> Result<Option<Remote<'_>>, Error> {
-    let head = repo.head()?;
-    let hash = head.peel_to_commit()?.id();
-    let mut ret = None;
+fn make_link(
+    url: GitUrl,
+    gitobj: GitObject,
+    path: String,
+    range: Option<(usize, usize)>,
+) -> Result<String, Error> {
+    let project = project_name(&url);
+    let host = url.host.ok_or(anyhow!("Unknown Git hosting site"))?;
+    let owner = url.owner.ok_or(anyhow!("Unknown repository owner"))?;
+    let mut link = String::new();
 
-    for r in repo.references()? {
-        if r.is_err() {
-            continue;
-        }
-
-        let r = r.unwrap();
-        if r.peel_to_commit()?.id() != hash {
-            continue;
-        }
-        if r.is_remote() {
-            let shorthand = std::str::from_utf8(r.shorthand_bytes())?;
-            let (remote, branch) = split_shorthand(shorthand);
-            if branch != "HEAD" {
-                ret = Some(repo.find_remote(remote)?);
-            }
-        }
+    match host {
+        h if h == "github.com" => link.push_str(format!("https://{h}/{owner}/{project}").as_str()),
+        _ => return Err(anyhow!("Unknown git hosting site")),
     }
 
+    match gitobj {
+        GitObject::Branch(name) | GitObject::Tag(name) => {
+            link.push_str(format!("/blob/{name}/{path}").as_str());
+            match range {
+                Some((begin, end)) if begin == end => link.push_str(format!("#L{begin}").as_str()),
+                Some((begin, end)) => link.push_str(format!("#L{begin}-L{end}").as_str()),
+                None => (),
+            }
+        }
+        GitObject::Commit(hash) => link.push_str(format!("/commit/{hash}").as_str()),
+    }
+
+    Ok(link)
+}
+
+fn get_remote_branch(repo: &Repository, wanted_remote: &str) -> Result<Option<GitObject>, Error> {
+    let head = repo.head()?;
+    let name = std::str::from_utf8(head.shorthand_bytes())?;
+    let branch = repo.find_branch(name, BranchType::Local)?;
+
+    match branch.upstream() {
+        Ok(upstream) => {
+            let shorthand = std::str::from_utf8(upstream.name_bytes()?)?;
+            let (_, branch) = split_shorthand(shorthand);
+            Ok(Some(GitObject::Branch(branch.to_string())))
+        }
+        Err(e) if e.code() == ErrorCode::NotFound => search_references(&repo, |r| {
+            if !r.is_remote() {
+                return None;
+            }
+            std::str::from_utf8(r.shorthand_bytes())
+                .ok()
+                .and_then(|shorthand| {
+                    let (remote, branch) = split_shorthand(shorthand);
+                    if remote == wanted_remote && branch != "HEAD" {
+                        Some(GitObject::Branch(branch.to_string()))
+                    } else {
+                        None
+                    }
+                })
+        }),
+        Err(e) => Err(e.into()),
+    }
+}
+
+fn search_references(
+    repo: &Repository,
+    f: impl Fn(Reference<'_>) -> Option<GitObject>,
+) -> Result<Option<GitObject>, Error> {
+    let head = repo.head()?;
+    let hash = head.peel_to_commit()?.id();
+    let ret = repo.references()?.find_map(|r| {
+        if r.is_err() {
+            return None;
+        }
+        let r = r.unwrap();
+        match r.peel_to_commit() {
+            Err(_) => None,
+            Ok(commit) if commit.id() != hash => None,
+            _ => f(r),
+        }
+    });
     Ok(ret)
+}
+
+fn split_shorthand(shorthand: &str) -> (&str, &str) {
+    let parts = shorthand.split('/').collect::<Vec<&str>>();
+    (parts[0], parts[1])
+}
+
+fn project_name(url: &GitUrl) -> String {
+    let parts = url.path.as_str().split('/').collect::<Vec<&str>>();
+    parts
+        .iter()
+        .last()
+        .unwrap()
+        .strip_suffix(".git")
+        .unwrap()
+        .to_string()
 }
